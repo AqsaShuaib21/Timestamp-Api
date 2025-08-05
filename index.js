e51bde837a8604f25275830ec9baa928585c7543
aqsashuaib21 <aqsashuaib077@gmail.com>
2025-08-05 12:50:25 +0530

    Implement timestamp API

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,10 +13,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", function (req, res) {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api/:date ?", function (req, res) {
+const dateParam=req.params.date;
+let date;
+//unix timestamp
+ if (/^\d+$/.test(dateParam)) {
+    date = new Date(Number(dateParam));
+  } else {
+    date = new Date(dateParam);
+  }
+//handle invalid date
+if(date.toString()==='Invalid Date'){
+  return res.json({error:"Invalid Date"});
+
+} 
+ res.json({unix:date.getTime(),
+    utc:date.toUTCString()
+  })
 });
 
 
